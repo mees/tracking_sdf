a69584608e7316af672d28c440bd84d18052a114
renumi <joel@renumi.de>
2014-07-08 17:34:28 +0200

    started to implement spatial derivative function

--- src/src/camera_tracking.cpp
+++ src/src/camera_tracking.cpp
@@ -21,29 +21,33 @@ void CameraTracking::camera_info_cb(const sensor_msgs::CameraInfoConstPtr &rgbd_
 	cout<<"read Camera Matrix"<<this->K<<endl;
 	this->cam_info.shutdown();
 }
-//todo: validate!
 void CameraTracking::project_camera_to_image_plane(Vector3d& camera_point, Vector2d& image_point){
 	//Eq. 2
 	Vector3d ij;
-	/*float fx = this->K(0,0);
-	float fy = this->K(1,1);
-	float cx = this->K(0,2);
-	float cy = this->K(1,2);*/
 	ij = this->K * camera_point;
-	image_point(0) = ij(0)/ij(2); //fx*(camera_point(0)/camera_point(2)) + cx;
-	image_point(1) = ij(1)/ij(2); //fy*(camera_point(1)/camera_point(2)) + cy;
+	image_point(0) = ij(0)/ij(2); 
+	image_point(1) = ij(1)/ij(2); 
 }
-//todo: validate
 void CameraTracking::project_world_to_camera(Vector3d& world_point, Vector3d& camera_point){
-      camera_point = this->rot_inv*world_point + this->rot_inv_trans;
-	//camera_point(0) = this->rot_inv(0,0) * world_point(0) + this->rot_inv(0,1) * world_point(1) + this->rot_inv(0,2) * world_point(2) + this->rot_inv_trans(0);
-	//camera_point(1) = this->rot_inv(1,0) * world_point(0) + this->rot_inv(1,1) * world_point(1) + this->rot_inv(1,2) * world_point(2) + this->rot_inv_trans(1);
-	//camera_point(2) = this->rot_inv(2,0) * world_point(0) + this->rot_inv(2,1) * world_point(1) + this->rot_inv(2,2) * world_point(2) + this->rot_inv_trans(2);
+	camera_point = this->rot_inv*world_point + this->rot_inv_trans;
 }
-//todo validate
-void CameraTracking::set_camera_transformation(Eigen::Matrix3d rot, Eigen::Vector3d trans){
+void CameraTracking::project_camera_to_world(Vector3d& camera_point, Vector3d& world_point){
+	world_point = this->rot*camera_point + this->trans;
+}
+void CameraTracking::set_camera_transformation(Eigen::Matrix3d& rot, Eigen::Vector3d& trans){
 	this->rot = rot;
 	this->rot_inv = rot.inverse();
 	this->trans= trans;
 	this->rot_inv_trans = -1* (rot_inv * trans);
+}
+void CameraTracking::get_partial_derivative(SDF& sdf, Eigen::Vector3d& camera_point, Eigen::Matrix<float, 6, 1>& SDF_derivative){
+	
+	Vector3d current_world_point;
+	//we use central difference
+	Vector3d before_world_point;
+	Vector3d behind_world_point;
+	float difference_size;
+	this->project_camera_to_world(camera_point, current_world_point);
+	//x derivative
+	
 }
